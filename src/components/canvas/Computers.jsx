2545dde7dcc29a314144fc5ead158a6cd414261c
Ashwin Gowda <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-05-30 16:30:05 +0530

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={4} groundColor='blue' />
+      <hemisphereLight intensity={0.85} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,7 +18,7 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={2} />
+      <pointLight intensity={1.5} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
